6f7f5fdfec18269d98b5155d2e1211444d9b04d0
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-04-09 13:30:33 +0200

    add page for news

--- js/news.js
+++ js/news.js
@@ -3,32 +3,28 @@ function articles(){
     let httpRequest = new XMLHttpRequest();
     // Function triggered at each change of state of the request
     httpRequest.onreadystatechange = function() {
-        let nameArticle= document.getElementsByClassName("headerID");
-        let title= document.getElementsByClassName("titre");
-        let content = document.getElementsByClassName("contenu");
         // When the server response has been received
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             // If the answer is valid
             if (httpRequest.status === 200) {
                 //variable get the response from server
                 let data = JSON.parse(httpRequest.responseText);
-                console.log(nameArticle);
-                //loop over the length of data (server response). The nameArticle, title and content variables take the value of data [i]
-                for(let i = 0; i< data.length; i++){
-                        console.log(i);
-                    nameArticle[i].innerText = data[i].id;
-                    title[i].innerText= data[i].titre;
-                    content[i].innerText= data[i].contenu;
+                //loop over themeArticle length of data (server response).
+                for(let i = 0; i < data.length; i++){
+                    let section = document.querySelector('section');
+                    section.innerHTML += '<div class="card text-center mt-5 sizeArticle"><div class="card-header">'+ data[i].id 
+                    + '</div><div class="card-body"><h5 class="card-title">' + data[i].titre +'</h5><p class="card-text">'
+                    + data[i].contenu +'</p></div><a class="btn colorButton sizeButton" href="#">En savoir plus</a></div>';
                 }
             } 
             // If an error has occurred
             else {
-                nameArticle.innerText = "Nous n'avons pas réussi à récupérer l'article";
+                section.innerText = "Nous n'avons pas réussi à récupérer l'article";
             }
         } 
         // The server has not yet responded
         else {
-            nameArticle.innerText = "Requête en cours";
+            section.innerText = "Requête en cours";
         }
     };
     // send request
